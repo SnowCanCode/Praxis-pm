f7e5c8bbc43d6f2627cab608896bf89c959855ff
SnowCanCode <parkerking0733@gmail.com>
2026-01-11 14:23:48 -0500

    Add Firebase authentication - signup, login, and dashboard

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,5 @@
 console.log('Flux initialized');
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('App starting...');
-    
-    // Get Started button
-    var getStartedBtn = document.querySelector('.btn-primary.btn-large');
-    if (getStartedBtn) {
-        getStartedBtn.addEventListener('click', function() {
-            console.log('Get Started clicked!');
-            alert('Sign up coming soon!');
-        });
-    }
-
-    // Learn More button
-    var learnMoreBtn = document.querySelector('.btn-secondary.btn-large');
-    if (learnMoreBtn) {
-        learnMoreBtn.addEventListener('click', function() {
-            console.log('Learn More clicked!');
-            alert('Documentation coming soon!');
-        });
-    }
-
-    console.log('Event listeners ready');
+    console.log('App ready');
 });
